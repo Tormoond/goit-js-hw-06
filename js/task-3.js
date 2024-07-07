603f8bfd9aa6d3508ab62e1b9429b70b3fbc4fe1
Your Name <112875685+Tormoond@users.noreply.github.com>
2024-07-07 15:28:17 +0200

    Home work 6

---
+++ js/task-3.js
@@ -0,0 +1,33 @@
+'use strict';
+console.log('-------TASK #-3 ------');
+console.log('Задача 3. Конструктор рядків');
+
+class StringBuilder {
+  #value;
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+  getValue() {
+    return this.#value;
+  }
+  padEnd(str) {
+    // this.#value = this.#value.padEnd(this.#value.length + 1, str);
+    this.#value = this.#value + str;
+  }
+
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
+}
+
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
